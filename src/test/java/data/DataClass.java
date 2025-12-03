8bce01ed159e37ea6214a226b6aae9ed0e3fdf02
Guirguis Nashat <gogonashatzaki@gmail.com>
2025-12-03 08:57:31 +0200

    Update DataClass.java

--- src/test/java/data/DataClass.java
+++ src/test/java/data/DataClass.java
@@ -2,12 +2,8 @@ package data;
 
 public class DataClass {
     public String SPOTIFY_BASE_URL = "https://api.spotify.com/v1/"; // base url
-    public String SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize\n" +
-            "?client_id=1a42254467e648019d04e15963d3e288\n" +
-            "&response_type=code\n" +
-            "&redirect_uri=http://127.0.0.1:8000/callback\n" +
-            "&scope=ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20app-remote-control%20streaming%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-follow-modify%20user-follow-read%20user-read-recently-played%20user-top-read%20user-read-playback-position%20user-library-modify%20user-library-read%20user-read-email%20user-read-private    \n";
-    public String ACCESS_TOKEN = "BQC7uVzy-ivAdhMOPFD9P21RJfKBNheIvJ3usXyrIQGhXzUvxdIPDu8illuQEzRdOvt0lOZQLKjYaw8M73VZZCnIIK9ynnkH1T1WxuayU7tzUOU911JgcRtm-wkUiMI4PpPOUMb-CSxQMQtTER-VdSJEzWfLEbxnTCnjBTfkDAiL0uFw_AHQPg-W8aIcOc4i9HsOI7qavlUMRgo9sWeEAXRKu9dv6iIucII3OSXwGFptx6CS75E77fCdeDDbrRNmds9E7xF1LNwfwfW9dbbd2N1yRypRs6_32WgD-_EFl8Z9TiBUfM6MqsgSEbp7lK-pAK2Bqb7fb_txN7PcQBFLq4Xs8qgWisqqBSWOTNfMkM96e9xIUdawQWKZqaW7pSjAjpw58MR-sqI";
+    public String SPOTIFY_AUTH_URL = "";
+    public String ACCESS_TOKEN = "";
     public String ACCESS_TOKEN_WITH_DIFFERENT_SCOPE = "";
     public String PLAYLIST_ID = "";
     public String RANDOM_PLAYLIST_NAME = "";
